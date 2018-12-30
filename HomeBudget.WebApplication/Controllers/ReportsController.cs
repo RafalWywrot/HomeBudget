b90506db89d001b5090406da2c4c29a055c71b73
Rafal Wywrot <Rafal.Wywrot@onet.pl>
2018-12-30 16:43:42 +0100

    add detailed report functionality

--- HomeBudget.WebApplication/Controllers/ReportsController.cs
+++ HomeBudget.WebApplication/Controllers/ReportsController.cs
@@ -16,10 +16,7 @@ namespace HomeBudget.WebApplication.Controllers
         }
         public ActionResult General()
         {
-            var model = new GeneralReportViewModel()
-            {
-                DateReport = DateTime.Now
-            };
+            var model = GetModelForGeneral(DateTime.Now);
             return View(model);
         }
         [HttpPost]
@@ -29,11 +26,33 @@ namespace HomeBudget.WebApplication.Controllers
             {
                 return View(model);
             }
-            DateTime date = (DateTime)model.DateReport;
+            var newModel = GetModelForGeneral(model.DateReport);
+            //DateTime date = (DateTime)model.DateReport;
+            //var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
+            //var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+            //var finanses = _unitOfWork.FinanceRepository.GetOverview(x => x.TimeEvent >= firstDayOfMonth && x.TimeEvent <= lastDayOfMonth).ToList();
+            //model.AnyCashOperation = false;
+            //if (finanses.Any())
+            //{
+            //    model.Revenues = finanses.Where(x => !x.Category.IsExpense).Select(x => x.Value).Sum();
+            //    model.Expenses = finanses.Where(x => x.Category.IsExpense).Select(x => x.Value).Sum();
+            //    model.AnyCashOperation = true;
+            //}
+            
+            
+            return View(newModel);
+        }
+        private GeneralReportViewModel GetModelForGeneral(DateTime dateTime)
+        {
+            DateTime date = (DateTime)dateTime;
             var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
             var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
             var finanses = _unitOfWork.FinanceRepository.GetOverview(x => x.TimeEvent >= firstDayOfMonth && x.TimeEvent <= lastDayOfMonth).ToList();
-            model.AnyCashOperation = false;
+            var model = new GeneralReportViewModel()
+            {
+                DateReport = dateTime,
+                AnyCashOperation = false
+            };
             if (finanses.Any())
             {
                 model.Revenues = finanses.Where(x => !x.Category.IsExpense).Select(x => x.Value).Sum();
@@ -42,7 +61,43 @@ namespace HomeBudget.WebApplication.Controllers
             }
             ModelState.Clear();
             ModelState.Remove("DateReport");
+            return model;
+        }
+        public ActionResult Detailed()
+        {
+            var model = GetModelForDetailed(DateTime.Now);
             return View(model);
+        }
+        [HttpPost]
+        public ActionResult Detailed(DetailedReportViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var newmodel = GetModelForDetailed(model.DateReport);
+            return View(newmodel);
+        }
+        private DetailedReportViewModel GetModelForDetailed(DateTime date)
+        {
+            var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
+            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+            var finanses = _unitOfWork.FinanceRepository.GetOverview(x => x.TimeEvent >= firstDayOfMonth && x.TimeEvent <= lastDayOfMonth).ToList();
+            var model = new DetailedReportViewModel()
+            {
+                DateReport = date,
+                AnyCashOperation = false,
+                Categories = _unitOfWork.CategoryRepository.GetOverview().ToList()
+            };
+            if (finanses.Any())
+            {
+                model.Revenues = Mapper.Map<List<RevenueViewModel>>(finanses.Where(x => !x.Category.IsExpense).ToList());
+                model.Expenses = Mapper.Map<List<RevenueViewModel>>(finanses.Where(x => x.Category.IsExpense).ToList());
+                model.AnyCashOperation = true;
+            }
+            ModelState.Clear();
+            ModelState.Remove("DateReport");
+            return model;
         }
     }
 }
---
+++ HomeBudget.WebApplication/Models/DetailedReportViewModel.cs
@@ -0,0 +1,17 @@
+﻿using HomeBudget.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeBudget.WebApplication.Models
+{
+    public class DetailedReportViewModel
+    {
+        public List<RevenueViewModel> Revenues { get; set; }
+        public List<RevenueViewModel> Expenses { get; set; }
+        public DateTime DateReport { get; set; }
+        public bool AnyCashOperation { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
