ab8eebc28bf8e5b9d71d4ca0972f37d433c0f8e9
Rafal <Rafal.Wywrot@onet.pl>
2019-05-30 22:24:33 +0200

    add controller tests

---
+++ HomeBudget.WebApplication.Tests/FakeUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using HomeBudget.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBudget.WebApplication.Tests
+{
+    public class FakeUnitOfWork : IUnitOfWork
+    {
+        public Domain.Interfaces.IRepository<HomeBudget.Database.Category> CategoryRepository => throw new NotImplementedException();
+
+        public Domain.Interfaces.IRepository<HomeBudget.Database.Finance> FinanceRepository => throw new NotImplementedException();
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HomeBudget.WebApplication.Tests/HomeControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using HomeBudget.WebApplication.Controllers;
+using HomeBudget.WebApplication.Models;
+using System.Web.Mvc;
+using Xunit;
+
+
+namespace HomeBudget.WebApplication.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_should_return_index_view()
+        {
+            var controller = new HomeController();
+            ViewResult result = (ViewResult)controller.Index();
+            Assert.Equal("Index", result.ViewName);
+        }
+        [Fact]
+        public void Unathorized_should_return_before_login_view()
+        {
+            var controller = new HomeController();
+            controller.ModelState.AddModelError("Register", "name required");
+            ViewResult result = (ViewResult)controller.Unathorized();
+            Assert.Equal("BeforeLogin", result.ViewName);
+        }
+    }
+}
