3455d9f84c4a0fc68d2ac860772410b1e968ffe0
Rafal <Rafal.Wywrot@onet.pl>
2019-05-30 22:25:48 +0200

    viewmodels tests

---
+++ HomeBudget.WebApplication.Tests/FinanceViewModelTests.cs
@@ -0,0 +1,56 @@
+﻿using HomeBudget.WebApplication.Models;
+using System;
+using Xunit;
+namespace HomeBudget.WebApplication.Tests
+{
+    public class FinanceViewModelTests
+    {
+        [Fact]
+        public void FinanceViewModel_validation_should_return_invalid_when_all_properties_empty()
+        {
+            var model = new FinanceViewModel();
+          
+            var modelValidation = new ValidationModel(model);
+            var valid = modelValidation.IsValid();
+            Assert.False(valid);
+            Assert.True(modelValidation.GetErrorMessages().Contains("Pole Nazwa jest wymagane."));
+        }
+        [Fact]
+        public void FinanceViewModel_validation_should_return_invalid_when_name_empty()
+        {
+            var model = new FinanceViewModel();
+
+            var modelValidation = new ValidationModel(model);
+            var valid = modelValidation.IsValid();
+            Assert.False(valid);
+            Assert.Contains("Nazwa jest wymagane.", modelValidation.GetErrorMessagesAsString());
+        }
+        [Fact]
+        public void FinanceViewModel_validation_should_return_invalid_when_time_event_lower_than_1900()
+        {
+            var model = new FinanceViewModel()
+            {
+                TimeEvent = new DateTime(1899, 12, 31)
+            };
+
+            var modelValidation = new ValidationModel(model);
+            var valid = modelValidation.IsValid();
+            Assert.False(valid);
+            Assert.Contains("Data przychodu musi być pomiędzy 01.01.1900", modelValidation.GetErrorMessagesAsString());
+        }
+        [Fact]
+        public void FinanceViewModel_validation_should_return_valid_model_when_name_is_not_null_and_timeEvent_is_higher_than_1900()
+        {
+            var model = new FinanceViewModel()
+            {
+                Name = "Finance",
+                TimeEvent = new DateTime(1900, 01, 01)
+            };
+
+            var modelValidation = new ValidationModel(model);
+            var valid = modelValidation.IsValid();
+            Assert.True(valid);
+            Assert.True(modelValidation.GetErrorMessages().Count == 0);
+        }
+    }
+}
---
+++ HomeBudget.WebApplication.Tests/LoginViewModelTests.cs
@@ -0,0 +1,57 @@
+﻿using HomeBudget.WebApplication.Models;
+using Xunit;
+
+namespace HomeBudget.WebApplication.Tests
+{
+    public class LoginViewModelTests
+    {
+        [Fact]
+        public void LoginViewModel_validation_should_return_invalid_when_all_properties_are_empty()
+        {
+            var model = new LoginViewModel();
+            var modelValidation = new ValidationModel(model);
+            var valid = modelValidation.IsValid();
+            Assert.False(valid);
+        }
+        [Fact]
+        public void LoginViewModel_validation_should_return_invalid_when_email_empty()
+        {
+            var model = new LoginViewModel()
+            {
+                Email = string.Empty
+            };
+
+            var modelValidation = new ValidationModel(model);
+            var valid = modelValidation.IsValid();
+            Assert.False(valid);
+            Assert.Contains("Email jest wymagane", modelValidation.GetErrorMessagesAsString());
+        }
+        [Fact]
+        public void LoginViewModel_validation_should_return_invalid_when_password_empty()
+        {
+            var model = new LoginViewModel()
+            {
+                Password = string.Empty
+            };
+
+            var modelValidation = new ValidationModel(model);
+            var valid = modelValidation.IsValid();
+            Assert.False(valid);
+            Assert.Contains("Hasło jest wymagane", modelValidation.GetErrorMessagesAsString());
+        }
+        [Fact]
+        public void LoginViewModel_validation_should_return_valid_when_data_is_valid()
+        {
+            var model = new LoginViewModel()
+            {
+                Email = "ao2@on.pl",
+                Password = "a"
+            };
+
+            var modelValidation = new ValidationModel(model);
+            var valid = modelValidation.IsValid();
+            Assert.True(valid);
+            Assert.Equal(string.Empty, modelValidation.GetErrorMessagesAsString());
+        }
+    }
+}
