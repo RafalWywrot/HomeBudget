3fd1e2efa36c33179a47e3f9efc67f3f96eaf38c
Rafal <Rafal.Wywrot@onet.pl>
2019-06-01 12:30:33 +0200

    handle admin general reports

--- HomeBudget.WebApplication/Controllers/AdminController.cs
+++ HomeBudget.WebApplication/Controllers/AdminController.cs
@@ -1,4 +1,4 @@
-﻿using HomeBudget.Database;
+using HomeBudget.Database;
 using HomeBudget.WebApplication.Models;
 using System;
 using System.Collections.Generic;
@@ -46,6 +46,49 @@ namespace HomeBudget.WebApplication.Controllers
             });
             _unitOfWork.SaveChanges();
             return RedirectToAction("ShowForCategoryGroup", "Admin", new { isExpense = category.IsExpense });
+        }
+
+        #region reports
+        public ActionResult Reports()
+        {
+            return View();
+        }
+        public ActionResult General()
+        {
+            var model = GetModelForGeneral(DateTime.Now);
+            ViewBag.ReportName = "General";
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult General(GeneralReportViewModel model)
+        {
+            ViewBag.ReportName = "General";
+            if (!ModelState.IsValid)
+                return View("General", model);
+
+            var newModel = GetModelForGeneral(model.DateReport);
+            return View(newModel);
+        }
+        private GeneralReportViewModel GetModelForGeneral(DateTime dateTime)
+        {
+            DateTime date = (DateTime)dateTime;
+            var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
+            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+            var finanses = _unitOfWork.FinanceRepository.GetOverview(x => x.TimeEvent >= firstDayOfMonth && x.TimeEvent <= lastDayOfMonth).ToList();
+            var model = new GeneralReportViewModel()
+            {
+                DateReport = dateTime,
+                AnyCashOperation = false
+            };
+            if (finanses.Any())
+            {
+                model.Revenues = finanses.Where(x => !x.Category.IsExpense).Select(x => x.Value).Sum();
+                model.Expenses = finanses.Where(x => x.Category.IsExpense).Select(x => x.Value).Sum();
+                model.AnyCashOperation = true;
+            }
+            ModelState.Clear();
+            ModelState.Remove("DateReport");
+            return model;
         }
     }
 }
